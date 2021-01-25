8ffb562646c42cb214fa6cba6a26d622552d2a1c
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-25 13:55:55 -0500

    commit tree 2e6a8dbf55e1cf19fbbecdc4b5ec2fab2f61878b

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  let newArr = [...drivers, name];
+  return newArr;
+}
+
+function prependDriver(name) {
+  let newArr = [name, ...drivers];
+  return newArr;
+}
+
+function removeLastDriver() {
+  return drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1);
+}
